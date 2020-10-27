4ab130123a99955874ae825501cffb7fa1afb8fd
Naia <43949390+NaiaIbanezdeGarayo@users.noreply.github.com>
2020-10-27 14:05:07 +0100

    Add files via upload

--- TEMA1/ej21.php
+++ TEMA1/ej21.php
@@ -1,1 +1,16 @@
 <?php
+$n = $_GET["numero"];
+
+function suma($n){
+    $s=0;
+    for ($x = 0; $x <= $n; $x++){
+        if ($x%2 == 0){
+            if ($s < 100){
+                $s +=  $x;
+            }else
+                return $s;
+        }
+    }
+    return $s;
+}
+echo suma($n);
